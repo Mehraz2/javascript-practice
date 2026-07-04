f3208d45cf3313ee6cdbc531bf7ed4f7a5162b45
Mehraz Hossain <rnjrohan786@gmail.com>
2026-07-04 11:55:35 +0600

    Initial commit

---
+++ function-RAKiB.js
@@ -0,0 +1,120 @@
+// function calculatteTotal (price1, price2, price3) {
+
+//     let result = price1 + price2 + price3;
+    
+//     console.log("Result = "  + result);
+    
+
+// }
+
+// calculatteTotal(100,200,50);
+
+
+
+
+// ===============================================
+
+
+// Task 1
+
+
+// function add( a, b ){
+//     return a + b;
+// }
+
+// let result = add (10 + 5);
+
+// console.log(results);
+
+
+// ===================================================
+
+
+
+
+// task 2
+
+
+// function greet (name) {
+//     console.log("Helllo " + name);
+    
+// }
+
+// greet ("Mehraz");
+// greet("Rohit")
+
+
+
+
+// ====================================================
+
+
+
+
+// task 3
+
+
+// function add (a, b) {
+//     console.log(a + b);
+    
+// }
+
+// add (50,30);
+
+
+// ==========================================================
+
+
+
+// task 4
+
+// function checkEvenOdd(num) {
+//     if (num % 2 === 0) return "Even";
+//     return "Odd";
+// }
+
+// console.log(checkEvenOdd(-20));
+
+
+
+// ===============================================================
+
+
+
+
+
+// task 4
+
+// positive / Negetive /  Ziro
+
+// function checkNumber (num) {
+//         if (num > 0) return  "Positive";
+//         if (num < 0) return "Negetive";
+//         return "Ziro"
+// }
+
+// console.time("Execution Time");
+// console.log(getNumberType(0));
+
+
+
+// ====================================================================
+
+
+
+// task 5
+
+// function marks(mark){
+
+
+// if (mark >= 80) return "A+";
+// if (mark >= 70) return "A";
+// if (mark >= 60) return "B";
+// return "Fail";
+  
+// }
+
+// console.log(marks(96));
+
+
+// ================================END FOR TODAY=========================
---
+++ function.js
@@ -0,0 +1,181 @@
+// function calculatteTotal (price1, price2, price3) {
+
+//     let result = price1 + price2 + price3;
+    
+//     console.log("Result = "  + result);
+    
+
+// }
+
+// calculatteTotal(100,200,50);
+
+
+
+
+// ===============================================
+
+
+//Task 1  ===== even odd
+
+// function checkNumber(num) {
+
+//     if (typeof num !== "number") return "Invalid Input";      
+//     if (num % 2===0) {
+//         return "even";
+//     } else {
+//         return "odd";
+//     }
+// }
+
+// console.log(checkNumber(20))
+// console.log(checkNumber(31))
+
+
+// ===================================================
+
+
+
+
+// task 2
+
+
+/// positive / Negetive /  Ziro
+
+// function checkNumber (num) {
+
+//         if (typeof num !== "number") return "Invalid Input";
+//         if (num > 0) return  "Positive";
+//         if (num < 0) return "Negetive";
+//         return "Ziro"
+// }
+
+
+// console.log(getNumberType(0));
+
+
+
+
+// ====================================================
+
+
+
+
+// // task 3
+
+// bigger Number
+
+// function add (a, b) {
+//     if (typeof a !== "number" || typeof b !== "number") 
+//         return "Invalid Input";
+
+//     console.log(a + b);
+    
+// }
+
+// add (50,30);
+
+
+// task 4
+
+// grade system
+
+// function getGrade (mark) {
+
+//     if (typeof marks !== "number") return "Invalid Input";
+//     if (mark >= 80) return "A+" ;
+//     if (mark >= 70) return "A";
+//     if (mark >= 60) return "B";
+//     return "fail"
+// }
+
+// console.log(getGrade(50));
+
+
+// ===========================================================
+
+// task 5
+
+// login check
+
+
+
+// function login (username, passward) {
+    
+//     if (!username || !password) return "Invalid Input";
+//     if (username === "mehraz" && passward === "01874404353" ) {
+//        return "Wellcome NEXT FUTURE ADVENTURE";
+//     } return "SORRY, YOU ARE IN WRONG CHAMBER"
+// }   
+
+// console.log(login ("mehraz", "01874404353"));
+
+
+// ===============================================================
+
+// task 6
+
+// -age check
+
+// function cheakAge (age) {
+      
+
+//     if (typeof age !== "number") return "Invalid Input";
+
+//     if (age >= 18) return "Eligible For Vote";
+
+//     return "Not Eligible";
+// }
+
+// console.log(cheakAge(19));
+
+
+// =================================================================
+
+// task 7
+
+
+// Discount system
+
+// function getDiscount(price) {
+    
+//     if (typeof price !== "number") return "Invalid Input";
+//     if (price >=1000) return "20% Discount";
+//     if (price >=500)  return "105 Discount";
+//     return "No Discount"
+// }
+
+// console.log(getDiscount(1000));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
