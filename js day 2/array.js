e76924fa320de5831609a8719ceea4d2d1776662
MehrAz <mirajul.hossain002@gmail.com>
2026-07-06 00:32:51 +0600

    Add JavaScript Day 2 - Arrays practice
    A collection of my daily JavaScript practice and learning journey. This repository documents my progress from basic concepts to advanced topics through hands-on coding exercises and projects.

---
+++ js day 2/array.js
@@ -0,0 +1,54 @@
+
+
+// =============== Fish print ====================
+
+// practice 1
+
+// let foods = ["Rice", "Fish", "Meat"];
+
+// console.log(foods[1]);
+
+// =============== Meat print ======================
+
+// practice 2
+
+
+// let foods = ["Rice", "Fish", "Meat"];
+
+// console.log(foods[2]);
+
+// =============== Length ============================
+
+
+// practice 3
+
+
+
+// let foods = ["Rice", "Fish", "Meat"];
+
+// console.log(foods[foods.length - 1]);
+
+// ================= Fish-এর জায়গায় Egg ================
+
+// practoce 4
+
+// let foods = ["Rice", "Fish", "Meat"];
+
+// foods[1] = "Egg"
+
+// console.log(foods);
+
+// ================= Last item print======================
+
+
+// practice 5
+
+
+// let foods = ["Rice", "Fish", "Meat"];
+
+// console.log(foods[2]);
+
+
+// ======================= end ===========================
+
+// =======================================================
