7b94df0fc2772dfc863624a1951efb7f85a40482
MehrAz <mirajul.hossain002@gmail.com>
2026-07-09 16:30:17 +0600

    Add files via upload

---
+++ js day 4/push.js
@@ -0,0 +1,84 @@
+// let fruits = ["Mango", "Apple"];
+
+// fruits.push ("cow");
+
+// console.log(fruits);
+
+
+// // output =  (3) ['Mango', 'Apple', 'cow']
+
+
+// // ======================= add to element========================
+
+
+
+// const numbers = [10, 20];
+
+// numbers.push("30,40,50,60");
+
+// console.log(numbers);
+
+
+// // output= 3) [10, 20, '30,40,50,60']
+
+
+// // ==============  orginal array=================================
+
+// const fruits = ["apple"];
+
+// fruits.push("orange");
+
+// console.log(fruits);
+
+// // output =  (2) ['apple', 'orange']
+
+
+// // =====================================================
+
+// // 1
+
+// const colors = ["Red", "Blue"];
+
+// colors.push ("Green");
+
+// console.log(colors);
+
+// // output =  (3) ['Red', 'Blue', 'Green']
+
+
+// // ================================================
+
+// // 2
+
+// const numbers = [5,10];
+
+// numbers.push("15,20,25");
+
+// console.log(numbers);
+
+// // output =  (3) [5, 10, '15,20,25']
+
+
+// // ==============================================
+
+// // 3
+
+// const students = [];
+
+// students.push("Rohit");
+
+// console.log(students);
+
+// // output =  ['Rohit'];
+
+// // ================================================
+
+// // 4
+
+// const fruits = ["apple"];
+
+// fruits.push();
+
+// console.log();
+
+// // output = nothing
