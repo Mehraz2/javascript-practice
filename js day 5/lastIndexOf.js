1c095a462d50848afe1e564ac54c39f35248c8b2
MehrAz <mirajul.hossain002@gmail.com>
2026-07-10 23:56:35 +0600

    Add files via upload

---
+++ js day 5/lastIndexOf.js
@@ -0,0 +1,62 @@
+// let fruits = ["Apple", "Mango", "Banana", "Mango"];
+
+// console.log(fruits.lastIndexOf("Mango"));
+
+// output = 3 
+
+// কারণ শেষ "Mango" index 3-এ আছে।
+
+// ==================================================
+
+// let numbers = [10, 20, 30, 20, 40];
+
+// console.log(numbers.lastIndexOf(20));
+
+
+// output = 3 
+
+//======================================================
+
+// let fruits = ["Apple", "Mango", "Banana"];
+
+// console.log(fruits.lastIndexOf("Orange"));
+
+// output = -1
+
+// কারণ "Orange" array-তে নেই।
+
+// ======================================================
+
+// indexOf() vs lastIndexOf()
+
+
+// indexOf()
+// let numbers = [10, 20, 30, 20, 40];
+
+// console.log(numbers.indexOf(20));
+
+// output = 1
+
+// ➡️ প্রথম 20-এর index।
+
+
+// lastIndexOf()
+
+// let numbers = [10, 20, 30, 20, 40];
+
+// console.log(numbers.lastIndexOf(20));
+
+// output = 3
+
+// ➡️ শেষ 20-এর index।
+
+// ===================================================
+
+// যদি value একবারই থাকে?
+
+// let numbers = [10, 20, 30, 40];
+
+// console.log(numbers.lastIndexOf(30));
+
+// output = 2
+
---
+++ js day 5/slice.js
@@ -0,0 +1,47 @@
+// let fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+// let result = fruits.slice(1, 3);
+
+// console.log(result);
+
+// output = (2) ['Mango', 'Banana']
+
+// কেন?
+// Index:   0        1         2          3
+//        Apple   Mango    Banana    Orange
+
+// Start = 1 ✅ (Mango নেবে)
+// End = 3 ❌ (Orange-এর আগ পর্যন্ত)
+
+// ===================================================
+
+
+// let numbers = [10, 20, 30, 40, 50];
+
+// console.log(numbers.slice(2, 5));
+
+// output =  [30, 40, 50]
+
+// ===================================================
+
+// let numbers = [10, 20, 30, 40, 50];
+
+// console.log(numbers.slice(2));
+
+// output = [30, 40, 50]
+
+// অর্থাৎ start থেকে শেষ পর্যন্ত।
+
+// ===================================================
+
+// let numbers = [10, 20, 30];
+
+// console.log(numbers.slice(0));
+
+// Output = [10, 20, 30]
+
+// এটা পুরো array-এর copy তৈরি করে।
+
+// =================================================
+
+
